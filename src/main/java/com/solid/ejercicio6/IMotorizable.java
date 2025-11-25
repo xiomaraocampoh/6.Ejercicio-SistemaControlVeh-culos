465099729e8dcc19d8857747570d6ad47b3b1754
Xiomara Ocampo <xocampo_165@cue.edu.co>
2025-11-25 14:03:04 -0500

    feat: ejercicio 6 Primera versión- sistema de vehículos aplicando ISP para separar motores de tracción humana

--- src/main/java/com/solid/ejercicio6/IMotorizable.java
+++
@@ -1,8 +0,0 @@
-package com.solid.ejercicio6;
-
-// [ISP]
-// Creamos una interfaz ESPECÍFICA solo para cosas con motor.
-// Así no obligamos a la bicicleta a implementar métodos que no necesita.
-interface IMotorizable {
-    void encenderMotor();
-}
